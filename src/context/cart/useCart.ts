ab06ad94fce51253b0eff415d944cd9c541aee84
keraneko <anbeplane@yahoo.co.jp>
2026-02-05 14:16:49 +0900

    fix: align cart context to null and fix error message

--- src/context/cart/useCart.ts
+++ src/context/cart/useCart.ts
@@ -6,7 +6,7 @@ export const useCart = () => {
   const context = useContext(CartContext);
 
   if (!context) {
-    throw new Error('useCustomer must be used within CustomerProvider'); //間違った使い方をしたらエラーを出す
+    throw new Error('useCart must be used within CartProvider'); //間違った使い方をしたらエラーを出す
   }
 
   return context;
