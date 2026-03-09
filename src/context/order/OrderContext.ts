d10c23aa627d720980ca2952578e1d6c0dc33d2c
keraneko <anbeplane@yahoo.co.jp>
2026-03-10 05:09:13 +0900

    fix:import OrderCustomerInput

--- src/context/order/OrderContext.ts
+++ src/context/order/OrderContext.ts
@@ -1,7 +1,7 @@
 import { createContext } from 'react';
 import type { CartItem } from '@/context/cart/CartContext';
-import type { OrderCustomerInput } from '@/context/customer/CustomerContext';
 import type { FulfillmentType } from '@/context/fulfillment/FulfillmentContext';
+import type { OrderCustomerInput } from '@/types/customer';
 
 export interface Order {
   fulfillment: FulfillmentType;
